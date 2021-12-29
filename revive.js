8e1de2720b13c3726fe45acce3bab39d512ca58b
Caio P <cpmadiab@gmail.com>
2021-12-29 13:27:03 -0300

    reset energy when revive

--- revive.js
+++ revive.js
@@ -4,7 +4,7 @@ let revive = function(target, client, username){
             client.say(target, `you're not dead KUKW`);
         }
         else{
-            connection.query(`UPDATE fightclubstats SET money = 0, health = 100, strength = 25 WHERE username = ?`, [username], function (error, results, fields){
+            connection.query(`UPDATE fightclubstats SET money = 0, health = 100, strength = 25, energy = 100 WHERE username = ?`, [username], function (error, results, fields){
                 client.say(target, `POGGING ${username} is back to life`);
             })
         }
