4bf155d6fd7cb8d969af9eefe1549b49778440f4
Caio P <cpmadiab@gmail.com>
2021-12-30 16:33:07 -0300

    add energy to score calculation and update $odds

--- score.js
+++ score.js
@@ -3,21 +3,25 @@ const { error } = require("tmi.js/lib/logger");
 let scoring = function(username, robpers){
     var strrob;
     var strusr;
+    var enrrob;
+    var enrusr;
     let myPromise = new Promise(function(myResolve, myReject) {
-        connection.query('SELECT username, strength FROM fightclubstats WHERE username IN (?, ?)', [robpers, username], function(error, results, fields){
+        connection.query('SELECT username, strength, energy FROM fightclubstats WHERE username IN (?, ?)', [robpers, username], function(error, results, fields){
             for (var i = 0; i < results.length; i++){
                 if (results[i].username == robpers){
                     strrob = results[i].strength;
+                    enrrob = results[i].energy;
                 }
                 if (results[i].username == username){
                     strusr = results[i].strength;
+                    enrusr = results[i].energy;
                 }
             }
             if (strrob == undefined || strusr == undefined){
                 myReject("Did not find them");  // when error
             }
             else {
-                var score = (strrob - strusr) / 100;
+                var score = ((enrrob + strrob) - (strusr + enrusr)) / 100;
                 myResolve(score); // when successful
             }
         })                
