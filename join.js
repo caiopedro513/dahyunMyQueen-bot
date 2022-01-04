be6b4e31ed804bdc9cb323b4fbe5d26f23842885
Caio P <cpmadiab@gmail.com>
2022-01-04 13:58:55 -0300

    remove useless stuff and now id is auto increment

--- join.js
+++ join.js
@@ -1,10 +1,8 @@
-let id = 26
 let join = function(target, client, username){
     connection.query(`SELECT username, id FROM fightclubstats WHERE username = ?`, [username], function (error, results, fields) {
         if (results.length === 0){
-            connection.query(`INSERT INTO fightclubstats VALUES(?, 100, 25, 1000, 100, ?)`, [username, id + 1], function(error, results, fields) {
+            connection.query(`INSERT INTO fightclubstats (name, health, strength, money, energy) VALUES (?, 100, 25, 1000, 100)`, [username], function(error, results, fields) {
                 client.say(target, `added OkayChamp`);
-                id += 1;
             })
         }
         else{
--- price.js
+++ price.js
@@ -1,19 +1,12 @@
-const { client } = require("tmi.js");
-const { error } = require("tmi.js/lib/logger");
-
 let pricing = function(username){
     var price = 0;
-    let prices = new Promise(function(myResolve, myReject) {
+    let prices = new Promise(function(myResolve) {
         connection.query(`SELECT money FROM fightclubstats WHERE username = ?`, [username], function(error, results, fields){
             if (results[0].money >= 150 ){
                 price = results[0].money * 0.35;
             }
-            else if (results[0].money < 150){
+            else{
                 price = 1;
-            }
-            
-            if (price == 0){
-                myReject("Error");  // when error
             }
             if (price > 0) {
                 myResolve(price); // when successful
