095a22ed2fc0dd864641eb66a7d58990cacfc8f6
Caio P <cpmadiab@gmail.com>
2022-01-05 12:03:42 -0300

    rewrite $gym

--- gym.js
+++ gym.js
@@ -1,56 +1,51 @@
 let pricing = require(`./price.js`).pricing;
+const { getUserStats } = require("./users/getUserStats");
+const { updateUserStats } = require("./users/updateUserStats");
+
 
 let gym = function(target, client, username){
-    connection.query(`SELECT username FROM fightclubstats WHERE username = ?`, [username], function (error, results, fields) {
-        if (results.length === 0){
-            client.say(target, `${username} is not even here KUKW but feel free to $join`);
+    getUserStats(username, username).then(function(userstats){
+
+        if (userstats.length === 0){
+            return client.say(target, `${username} is not even here KUKW but feel free to $join`);
         }
-        else{
-            connection.query(`SELECT health FROM fightclubstats WHERE username = ?`, [username], function (error, results, fields){
-                if (results[0].health <= 0){
-                    client.say(target, `KUKW Dead people can't workout try $revive`);
-                }
-                else{
-                    connection.query(`SELECT energy FROM fightclubstats WHERE username = ?`, [username], function(error, results, fields){
-                        if (results[0].energy <= 0){
-                            client.say(target, `KUKW you're too tired to workout`);
-                        }
-                        if (results[0].energy > 0){
-                            pricing(username).then(function(price){
-                                if (price == 1){
-                                    connection.query(`SELECT money FROM fightclubstats WHERE username = ?`, [username], function (error, results, fields){
-                                        if (results[0].money < 75){
-                                            client.say(target, `${username} you got beat up at the gym after trying not to pay peepoSmash`);
-                                            connection.query(`UPDATE fightclubstats SET health = health - 30 WHERE username = ?`, [username], function (error, results, fields){
-                                            })
-                                            connection.query(`SELECT health FROM fightclubstats WHERE username = ?`, [username], function(error, results, fields){
-                                                if (results[0].health <= 0){
-                                                    client.say(target, `reeferSad ${username} died`);
-                                                }
-                                            })
-                                        }
-                                        else{
-                                            connection.query(`UPDATE fightclubstats SET money = money - 75, strength = strength + 15, energy = energy - 10 WHERE username = ?`, [username], function(error, results, fields){
-                                                connection.query(`SELECT money FROM fightclubstats WHERE username = ?`, [username], function(error, results, fields){
-                                                    client.say(target, `BillyApprove ${username} you worked out really hard great job +15💪 💰${results[0].money}`);
-                                                })
-                                            })
-                                        }
-                                    })
-                                }
-                                if (price > 1){
-                                    connection.query(`UPDATE fightclubstats SET money = money - ?, strength = strength + 15, energy = energy - 10 WHERE username = ?`, [price, username], function(error, results, fields){
-                                        connection.query(`SELECT money FROM fightclubstats WHERE username = ?`, [username], function(error, results, fields){
-                                            client.say(target, `BillyApprove ${username} you worked out really hard great job +15💪 💰${results[0].money}`);
-                                        })
-                                    })
-                                }
-                            })
+
+        if (userstats.health <= 0){
+            return client.say(target, `KUKW Dead people can't workout try $revive`);
+        }
+
+        if (userstats.energy <= 0){
+            return client.say(target, `KUKW you're too tired to workout`);
+        }
+
+        pricing(username).then(function(price){
+
+            if (price == 1){
+
+                if (userstats.money < 75){
+                    client.say(target, `${username} you got beat up at the gym after trying not to pay peepoSmash`);
+                    updateUserStats(username, -30, 0, 0, 0);
+                    getUserStats(username, username).then(function(userstats){
+                        if (userstats.health <= 0){
+                            return client.say(target, `reeferSad ${username} died`);
                         }
                     })
                 }
-            })
-        }
+
+                else{
+                    updateUserStats(username, 0, -10, -75, 15);
+                    getUserStats(username, username).then(function(userstats){
+                        return client.say(target, `BillyApprove ${username} you worked out really hard great job +15💪 💰${userstats.money}`);
+                    })
+                }
+            }
+            if (price > 1){
+                updateUserStats(username, 0, -10, -price, 15);
+                getUserStats(username, username).then(function(userstats){
+                    return client.say(target, `BillyApprove ${username} you worked out really hard great job +15💪 💰${userstats.money}`);
+                })
+            }
+        })
     })
 }
 module.exports = {gym};
