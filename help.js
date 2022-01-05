544192ec377d2e6cce173c59b2bde6dbcbde2e27
Caio P <cpmadiab@gmail.com>
2022-01-05 11:03:00 -0300

    update $help

--- help.js
+++ help.js
@@ -1,7 +1,8 @@
 let help = function(target, client){
     client.say(target, `You start with 25💪 $250 ⚡100 and ❤100 you can $fight with other people to get money you also get 
-    $5 every 30min as well as ⚡10, and if you're not dead you get ❤5 every 30min you can workout by going to the $gym for $75 wich will make you stronger
-    you can $heal for $100 to get more health and if you want to know the odds just type $odds, to heal a bit and also restore energy you can $buy stuff in the $store`);
+    $5 every 30min as well as ⚡10, and if you're not dead you get ❤5 every 30min you can workout by going to the $gym for $75 wich will make you stronger 
+    to get stronger you can also fight with someone stronger than, you doing that will give you +10💪 you can $heal for $100 to get more health and if you want
+     to know the odds just type $odds, to heal a bit and also restore energy you can $buy stuff in the $store`);
 }
 
 module.exports = {help}
