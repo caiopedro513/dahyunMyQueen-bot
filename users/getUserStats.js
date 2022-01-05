b00ddea30ceb3f60f9360067fd2e477548943f8e
Caio P <cpmadiab@gmail.com>
2022-01-05 10:52:39 -0300

    fixed gerUserStats

--- users/getUserStats.js
+++ users/getUserStats.js
@@ -4,16 +4,26 @@ let getUserStats = function(target, username){
     let usrstats = new Promise (function(myResolve, myReject) {
         if (!target){
             connection.query(`SELECT * FROM fightclubstats WHERE health > 0 AND username != ? ORDER BY RAND() LIMIT 1`,[username], function(error, results, fields){
-                myResolve(results[0]); // everything from the random person
+                if (results != undefined){
+                    if (results.length === 0){
+                        myReject(`${target} is not even here KUKW but feel free to $join`);
+                    }
+
+                    else{
+                      myResolve(results[0]);
+                    }
+                }
             })
         }
         else{
             connection.query(`SELECT * FROM fightclubstats WHERE username = ?`, [target], function(error, results, fields){
-                if (results[0] == undefined){
-                    myReject("The person is not in");
-                }
-                else {
-                    myResolve(results[0]);
+                if (results != undefined){
+                    if (results.length === 0){
+                        myReject("The person is not in");
+                    }
+                    else {
+                        myResolve(results[0]);
+                    }
                 }
             })       
         }
