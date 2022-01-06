68db733abb900a7b4b52d7dc0fe7daf0b1c6ad70
Caio P <cpmadiab@gmail.com>
2022-01-06 17:15:07 -0300

    remove ugly bars

--- stats.js
+++ stats.js
@@ -19,8 +19,8 @@ let stats = function(target, client, username, mssg){
         if (userstats.length == 0){
             return client.say(target, `KUKW ${tgtpers} is not even feel free to $join`);
         }
-        return client.say(target, `|Username : ${userstats.username} | Money : ${userstats.money} | Energy : ${userstats.energy} 
-        | Health: ${userstats.health} | Strength : ${userstats.strength}|`);
+        return client.say(target, `Username : ${userstats.username} | Money : ${userstats.money} | Energy : ${userstats.energy} 
+        | Health: ${userstats.health} | Strength : ${userstats.strength}`);
     })
 }
 
