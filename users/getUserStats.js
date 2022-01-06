909b6ee27e9cdf4645d4fd9de6358eca0d93e7bd
Caio P <cpmadiab@gmail.com>
2022-01-06 15:17:18 -0300

    $fight doesnt choose tired people anymore

--- users/getUserStats.js
+++ users/getUserStats.js
@@ -3,7 +3,7 @@ const { username } = require("tmi.js/lib/utils");
 let getUserStats = function(target, username){
     let usrstats = new Promise (function(myResolve, myReject) {
         if (!target){
-            connection.query(`SELECT * FROM fightclubstats WHERE health > 0 AND username != ? ORDER BY RAND() LIMIT 1`,[username], function(error, results, fields){
+            connection.query(`SELECT * FROM fightclubstats WHERE health > 0 AND energy > 0 AND username != ? ORDER BY RAND() LIMIT 1`,[username], function(error, results, fields){
                 if (results != undefined){
                     if (results.length === 0){
                         myReject(`${target} is not even here KUKW but feel free to $join`);
