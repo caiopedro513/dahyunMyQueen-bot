94cd9d632d8feb96925be67774f976badd529e87
Caio P <cpmadiab@gmail.com>
2022-02-27 22:52:01 -0300

    adds smartness when join

--- join.js
+++ join.js
@@ -1,7 +1,7 @@
 let join = function(target, client, username){
     connection.query(`SELECT username, id FROM fightclubstats WHERE username = ?`, [username], function (error, results, fields) {
         if (results.length === 0){
-            connection.query(`INSERT INTO fightclubstats (username, health, strength, money, energy, fat) VALUES (?, 100, 25, 1000, 100, 10)`, [username], function(error, results, fields) {
+            connection.query(`INSERT INTO fightclubstats (username, health, strength, money, energy, fat, smartness) VALUES (?, 100, 25, 1000, 100, 10, 10)`, [username], function(error, results, fields) {
                 client.say(target, `added OkayChamp`);
             })
         }
