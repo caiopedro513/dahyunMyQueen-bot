9643e69c921284f37142381d35049c85d184a45d
Caio P <cpmadiab@gmail.com>
2022-06-30 21:38:48 -0300

    creates fight() for dungeon

---
+++ dungeonf/dgfight.js
@@ -0,0 +1,53 @@
+let scoringd = require(`./dgscoring.js`).scoringd;
+let getMobStats = require(`./getMobStats.js`).getMobStats;
+let getUserStats = require(`../users/getUserStats.js`).getUserStats;
+let updateUserStats = require(`../users/updateUserStats.js`).updateUserStats;
+let resetUserStats = require(`../users/resetUserStats.js`).resetUserStats;
+let diceroll = require(`../functions.js`).diceroll;
+
+let dgfight = function(username, tgtmob, mssg, client, target){
+    let numrol = diceroll();
+    getUserStats(username, username).then(function(userstats) {
+        if (userstats.health <= 0){
+            return client.say(target, `KUKW you can't fight you're dead but you can $revive`);
+        }
+        if (userstats.energy <= 0){
+            return client.say(target, `KUKW ${username} you're too tired`);
+        }
+    }).catch(function(myReject) {
+        return client.say(target, `KUKW ${username} you are not in try to $join`);
+    })
+    getMobStats(tgtmob).then(function(mobStats){}).catch(function(myReject){
+        return client.say(target, myReject);
+    })
+
+    scoringd(username, tgtmob).then(function(score){
+        let dmg = abs(score) / 100;
+
+        if (score == 0){
+            return client.say(target, `KUKW both ${username} and ${tgtmob} passed out while fighting`);
+        }
+
+        if (score > 0){
+            dmg = dmg / 2;
+            chances = 100 - mobStats.probability
+            loot = mobStats.money
+            if (numrol >= chances){
+                loot = loot * mobStats.multiplier
+            }
+            updateUserStats(username, -dmg, -25, loot, 10, 0, 0).then(function(){
+                return client.say(target, `BASED ${username} beat ${tgtmob} up and got []`)
+            })
+        }
+        
+        updateUserStats(username, -dmg, -30, 0, 20, 0, 0).then(function(){
+            client.say(target, `LULE ${username} is garbage and lost to ${tgtmob}`);
+        })
+
+        getUserStats(username, username).then(function(userStats){
+            if (userStats.health <= 0){
+                return client.say(target, `FeelsWeakMan ${username} died`)
+            }
+        })
+    })
+}
