9e4f448140f2e465dbf5c30c276b3a9a8d3ff732
Caio P <cpmadiab@gmail.com>
2022-12-15 09:29:49 -0300

    Rewrite heal with up to date functions and stuff

--- heal.js
+++ heal.js
@@ -1,47 +1,39 @@
 let pricing = require(`./price.js`).pricing;
 let updateSlotStats = require(`./slotmachine/updateSlotStats.js`).updateSlotStats;
+let getUserStats = require(`./users/getUserStats`).getUserStats;
+let updateUserStats = require(`./users/updateUserStats`).updateUserStats;
+const user = require(`./user.js`).user;
 
 let heal = function(target, client, username){
-    connection.query(`SELECT username FROM fightclubstats WHERE username = ?`, [username], function (error, results, fields) {
-        if (results.length === 0){
-            client.say(target, `${username} is not even here KUKW but feel free to $join`);
+    getUserStats(username, username).then(function(userstats){}).catch(function(myReject){
+        return client.say(target, `KUKW ${username} you are not in try to $join`);
+    })
+    getUserStats(username, username).then(function(userstats){
+        if (userstats.health <= 0){
+            return client.say(target, `KUKW Dead people can't go to hospital try $revive`);
         }
-        else{
-            connection.query(`SELECT health FROM fightclubstats WHERE username = ?`, [username], function (error, results, fields){
-                if (results[0].health <= 0){
-                    client.say(target, `KUKW Dead people can't go to hospital try $revive`);
-                }
-                else{
-                    pricing(username).then(function(price){
-                        if (price < 100){ 
-                            connection.query(`SELECT money FROM fightclubstats WHERE username = ?`, [username], function (error, results, fields){
-                                if (results[0].money < 100){
-                                    client.say(target, `doctor killed you after seeing you didnt have money to pay him PepeLoser Stab`);
-                                    connection.query(`UPDATE fightclubstats SET health = 0 WHERE username = ?`, [username], function (error, results, fields){
-                                    })
-                                }
-                                else{
-                                    connection.query(`UPDATE fightclubstats SET money = money - 100, health = health + 40 WHERE username = ?`, [username], function(error, results, fields){
-                                        connection.query(`SELECT health, money FROM fightclubstats WHERE username = ?`, [username], function(error, results, fields){
-                                            updateSlotStats(100);
-                                            client.say(target, `twiceLove you got healed by ❤40 and now has [${results[0].health}❤] [${results[0].money}💰]`);
-                                        })
-                                    })
-                                }
-                            })
-                        }
-                        if (price >= 100){
-                            connection.query(`UPDATE fightclubstats SET money = money - ?, health = health + 40 WHERE username = ?`, [price, username], function(error, results, fields){
-                                connection.query(`SELECT health, money FROM fightclubstats WHERE username = ?`, [username], function(error, results, fields){
-                                    updateSlotStats(price);
-                                    client.say(target, `twiceLove you got healed by ❤40 and now has [${results[0].health}❤] [${results[0].money}💰]`);
-                                })
-                            })
-                        }
+        pricing(username).then(function(price){
+            if (price < 100){ 
+                if (userstats.money < 100){
+                    user.setHealth(username, -userstats.health);
+                    return client.say(target, `doctor killed you after seeing you didnt have money to pay him PepeLoser Stab`);
+                }//username, health, energy, money, strength, fat, smartness
+                updateUserStats(username, 40, 0, -100, 0, 0, 0).then(function(){
+                    getUserStats(username, username).then(function(userstats){
+                        updateSlotStats(100);
+                        return client.say(target, `twiceLove you got healed by ❤40 and now has [${userstats.health}❤] [${userstats.money}💰]`);
                     })
-                }
-            })
-        }
+                })
+            }
+            if (price >= 100){
+                updateUserStats(username, 40, 0, -price, 0, 0, 0).then(function(){
+                    getUserStats(username, username).then(function(userstats){
+                        updateSlotStats(price);
+                        return client.say(target, `twiceLove you got healed by ❤40 and now has [${userstats.health}❤] [${userstats.money}💰]`);
+                    })
+                })
+            }
+        })
     })
 }
 module.exports = {heal};
